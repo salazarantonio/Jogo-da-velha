17210b198923408aa861f1434cc8b48b0a8f5e42
salazarantonio <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>
2026-06-28 21:39:06 -0300

    Adicionado controlador (logica simples) do jogo da velha

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void) {
+    char tabuleiro[3][3], player;
+    int linha, coluna, jogadaValida, estadoJogo = 1; // 1 ativo, 0 inativo
+
+    printf("Player 1 - x, Player 2 - O\n\n");
+
+    // Criar tabuleiro
+    inicializar(tabuleiro);
+    player = 'X';
+
+    while (estadoJogo) {
+        mostrarT(tabuleiro);
+
+        // Realizar a jogada
+        do {
+            printf("Vez de player %c\n", player);
+            printf("Digite a linha e coluna de 0-2 para jogar: ");
+            scanf("%d %d", &linha, &coluna);
+
+            jogadaValida = realizarJogada(tabuleiro, linha, coluna, player);
+
+            if (!jogadaValida) {
+                printf("Posicao invalida! Digite novamente.");
+            }
+        } while (!jogadaValida);
+
+        // Verificar se o jogador venceu
+        if (vitoria(tabuleiro, player)) {
+            mostrarT(tabuleiro);
+            printf("Jogador %c venceu!\n", player);
+            // Encerrar
+            estadoJogo = 0;
+        }
+            
+        // Verificar se o jogo empatou
+        if (empate(tabuleiro)) {
+            mostrarT(tabuleiro);
+            printf("Ninguem ganhou. Empate!\n");
+            // Encerrar
+            estadoJogo = 0;
+        }
+
+        // Trocar player
+        player = trocarP(player);
+        
+    }
+    return 0;
+}
